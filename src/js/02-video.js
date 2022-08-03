dcb05aedc9196396a982f0826c9a8dbb62a6f741
lerka22eklerka <94803605+lerka22eklerka@users.noreply.github.com>
2022-08-03 18:55:52 +0300

    task_two

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import vimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
+    
+const iframe = document.querySelector('iframe');
+const player = new vimeoPlayer(iframe);
+
+// console.log(player);
+player.on('timeupdate', throttle(onTimePlayer, 1000));
+
+function onTimePlayer(data) {
+    // console.log(data);
+    const { seconds } = data;
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));    
+};
+
+const getTime = localStorage.getItem('videoplayer-current-time');
+const saveTime = JSON.parse(getTime);
+
+// console.log(saveTime);
+ 
+player.setCurrentTime(JSON.parse(saveTime));
+
+
 

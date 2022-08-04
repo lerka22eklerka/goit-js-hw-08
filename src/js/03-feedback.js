3b41e825af5c3a0750e85a1e1e2b63ed7eeeb28f
lerka22eklerka <94803605+lerka22eklerka@users.noreply.github.com>
2022-08-04 15:12:39 +0300

    update_task_three

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,42 @@
 import throttle from 'lodash.throttle';
-const formRef = document.querySelector('.feedback-form');
-const textAreaRef = document.querySelector('textarea'); 
-const inputRef = document.querySelector('input');
 
-const formData = {};
+const formRef = document.querySelector('.feedback-form');
+const textAreaRef = document.querySelector('textarea');
+const inputRef = document.querySelector('input');
 
 formRef.addEventListener('input', throttle(onTextAreaInput, 500));
 formRef.addEventListener('submit', onFormSubmit);
 
-onSaveDataInfo();
+if (localStorage.getItem('feedback-form-state')) {
+  onSaveDataInfo();
+}
+let formData = localStorage.getItem('feedback-form-state')
+  ? JSON.parse(localStorage.getItem('feedback-form-state'))
+  : {};
 
 function onTextAreaInput(event) {
-    formData[event.target.name] = event.target.value;
-
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-    // console.log(formData);
-};
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
 
 function onFormSubmit(event) {
-    event.preventDafault();
-    event.target.reset();
-
-    localStorage.removeItem('feedback-form-state');
-    
-
-};
+  event.preventDefault();
+  const savesData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  console.log(savesData);
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
 
 function onSaveDataInfo() {
-    const saveFormData = localStorage.getItem('feedback-form-state');
-    const parsData = JSON.parse(saveFormData);
-    console.log(parsData);
-    if (saveFormData) {
-        textAreaRef.value = parsData.message;
-        inputRef.value = parsData.email;
-        // console.log(saveFormData);
-    }
-
+  const saveFormData = localStorage.getItem('feedback-form-state');
+  const parsData = JSON.parse(saveFormData);
+  console.log(parsData);
+  if (parsData) {
+    inputRef.value = parsData.email ? parsData.email : ``;
+    textAreaRef.value = parsData.message ? parsData.message : ``;
+    // console.log(saveFormData);
+  }
 }
+
 
 
